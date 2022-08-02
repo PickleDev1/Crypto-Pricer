8044ccfa23220a8b22fb86afd169c542845d7bcc
PickleDev1 <96743952+PickleDev1@users.noreply.github.com>
2022-08-02 15:27:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const getCirculation = require('./Functions/getCirculating')
+const getPrice = require('./Functions/getPrice')
+const getData = require('./Functions/getData')
+module.exports = {getCirculation, getPrice, getData}
